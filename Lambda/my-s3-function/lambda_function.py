3b8c5afaa21ce7beb37d3d0b59ba7c8d4fce55fb
irmadong <44555457+irmadong@users.noreply.github.com>
2021-11-13 16:25:58 -0500

    update indexlambda

--- Lambda/my-s3-function/lambda_function.py
+++ Lambda/my-s3-function/lambda_function.py
@@ -1,6 +1,5 @@
 import json
 import boto3
-# from botocore.vendored import requests
 import requests
 from datetime import datetime
 from requests_aws4auth import AWS4Auth
@@ -10,25 +9,17 @@ from requests_aws4auth import AWS4Auth
 def detectLabels(photo, bucket):
     client=boto3.client('rekognition')
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
-    print('rekognition', response)
-    # "MaxLabels": 10, "MinConfidence": 75 (Optional?)
-    
+    print('rekognition', response)    
     return response
 
 def retriveMetadata(photo, bucket):
     client = boto3.client('s3')
     response = client.head_object(Bucket=bucket, Key=photo)
-    #to do where to handle custom label?
-    print("the metadata is ")
-    print(response['Metadata'])
     return response['Metadata']
     
     
 def parseForES(bucket, photo, labels):
-    # labels = []
-    
-    # for rec in resources['Labels']:
-    #     labels.append(rec['Name'])
+
     jsonObject = {
         "objectKey" : photo,
         "bucket" : bucket,
@@ -40,10 +31,8 @@ def parseForES(bucket, photo, labels):
     
 def indexToES(document):
     host = "https://search-photos-zo25n66ahw2zn46y2tx3vn4pha.us-east-1.es.amazonaws.com/photos/_doc"
-    # host = "https://search-searchphoto2-7cg5u7mox3a5md4ddbjemlscpe.us-east-1.es.amazonaws.com"
     index = "photos"
-    # url = host + '/' + index + '/' + 'lambda-type'
-    # url = host + '/' + index
+
     region = 'us-east-1'
     service = 'es'
     headers = { "Content-Type": "application/json" }
@@ -66,9 +55,7 @@ def lambda_handler(event, context):
     
     bucket = event['Records'][0]["s3"]["bucket"]["name"]
     photo = event['Records'][0]["s3"]["object"]["key"]
-    print("photo is ", photo)
     labels = detectLabels(photo, bucket)
-    print('labels', labels)
     metadata = retriveMetadata(photo, bucket)
     if 'customlabels' in metadata.keys():
         a1 = [metadata['customlabels']]
@@ -77,9 +64,7 @@ def lambda_handler(event, context):
             
     for i in range(len(labels['Labels'])):
         a1.append(labels['Labels'][i]['Name'])
-    print("the labesl in total")
-    print(a1)
-    
+
     
     document = parseForES(bucket, photo, a1)
     print("document is", document)
