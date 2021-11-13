c292be859e29ff4b24e28b3e7bfa4761e7daa947
irmadong <44555457+irmadong@users.noreply.github.com>
2021-11-13 16:34:22 -0500

    update search

--- Lambda/search-photos/lambda_function.py
+++ Lambda/search-photos/lambda_function.py
@@ -18,37 +18,11 @@ credentials = boto3.Session().get_credentials()
 service = 'es'
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-
-
-def close(session_attributes, fulfillment_state, message):
-    response = {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': fulfillment_state,
-            'message': message
-        }
-    }
-    
-    return response
-
     
 def lambda_handler(event, context):
-    print(event)
     os.environ['TZ'] = 'America/New_York'
     time.tzset()
-
-    print("this is event:")
-    print(event)
     q1 = event["queryStringParameters"]['q']
-    # tmp = q1.split()
-    # q1 = '+'.join(tmp)
-    
-    """
-        
-    
-    """
-    # q1 = event['inputTranscript']
     labels = get_labels(q1)
     if len(labels) != 0:
         img_paths = search_intent('b2-photos-storage',labels)
@@ -88,10 +62,21 @@ def get_labels(query):
                 labels.append(value)
     return labels 
 
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+    
+    return response
+
 
    
 def dispatch(intent_request):
-    #logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
     intent_name = intent_request['currentIntent']['name']
     return search_intent(intent_request)
 
@@ -101,29 +86,17 @@ def search_intent(bucket,labels):
     url = host+'/_search?q='
     resp = []
     for label in labels:
-        print("label is")
-        print(label)
         url2 = url+label
-        print("url2 is")
-        print(url2)
         resp.append(requests.get(url2,auth=awsauth).json())
-    
-    print("resp is:")
-    print (resp)
-    #to do wrong implementation
     output = []
     for r in resp:
         if 'hits' in r:
              for val in r['hits']['hits']:
                 key = val['_source']['objectKey']
-                print('key is ')
-                print(key)
+
                 if key not in output:
                     photoURL = "https://{0}.s3.amazonaws.com/{1}".format(bucket,key)
-                    #todo object key with the link
                     output.append(photoURL)
-    print("output is")
-    print(output)
 
     return output
 
